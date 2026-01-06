49cead12fae9e8eb1847cfe81f6a747806837d5d
AKS-2003 <akshaya@farmonaut.com>
2026-01-06 11:25:52 +0530

    to create the relase apk with hospital helpline page

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -6,7 +6,7 @@ import 'sos_screen.dart';
 import 'emergency_contacts_screen.dart';
 import 'helpline_screen.dart';
 import 'record_screen.dart';
-import 'track_me_screen.dart';
+import 'track_me_screen2.dart' as track_screen;
 import 'profile_screen.dart';
 import '../services/supabase_service.dart';
 
@@ -415,7 +415,7 @@ class DashboardScreenState extends State<DashboardScreen> {
                         onTap: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => const TrackMeScreen()),
+                            MaterialPageRoute(builder: (context) => const track_screen.TrackMeScreen()),
                           );
                         },
                         image: "assets/location.png"
